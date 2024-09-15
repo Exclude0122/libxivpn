ad29637f9a3e9f978af74a48ef9d9fc7943fc0f5
Exclude0122 <173652916+Exclude0122@users.noreply.github.com>
2024-09-14 22:21:09 -0400

    panic recover

--- libxivpn.go
+++ libxivpn.go
@@ -7,16 +7,17 @@ package main
 */
 import "C"
 import (
-	"strings"
 	"fmt"
+	"strings"
 
-	"github.com/xtls/xray-core/core"
-	_ "github.com/xtls/xray-core/main/distro/all"
-	"github.com/xjasonlyu/tun2socks/v2/engine"
+	"bufio"
+	"os"
 	"strconv"
 	"time"
-	"os"
-	"bufio"
+
+	"github.com/xjasonlyu/tun2socks/v2/engine"
+	"github.com/xtls/xray-core/core"
+	_ "github.com/xtls/xray-core/main/distro/all"
 )
 
 var xrayServer core.Server
@@ -37,32 +38,39 @@ func libxivpn_start(cConfig *C.char, socksPort C.int, fd C.int) *C.char {
 
 	log(fmt.Sprintln("start", config, socksPort, fd))
 
+	defer func() {
+		r := recover()
+		if r != nil {
+			log(fmt.Sprintf("panic: %v", r))
+		}
+	}()
+
 	// xray
 
 	xrayConfig, err := core.LoadConfig("json", strings.NewReader(config))
 	if err != nil {
 		log("libxivpn_start xray1: " + err.Error())
-		return C.CString("libxivpn_start load config: " + err.Error());
+		return C.CString("libxivpn_start load config: " + err.Error())
 	}
 
 	xrayServer, err = core.New(xrayConfig)
 	if err != nil {
 		log("libxivpn_start xray2: " + err.Error())
-		return C.CString("libxivpn_start create core: " + err.Error());
+		return C.CString("libxivpn_start create core: " + err.Error())
 	}
 
 	err = xrayServer.Start()
 	if err != nil {
 		log("libxivpn_start xray3: " + err.Error())
-		return C.CString("libxivpn_start start core: " + err.Error());
+		return C.CString("libxivpn_start start core: " + err.Error())
 	}
 
 	// tun2socks
 	engine.Insert(&engine.Key{
-		MTU: 1400,
-		Proxy: "socks5://10.89.64.1:" + strconv.Itoa(int(socksPort)),
-		Device: "fd://" + strconv.Itoa(int(fd)),
-		LogLevel: "debug",
+		MTU:        1400,
+		Proxy:      "socks5://10.89.64.1:" + strconv.Itoa(int(socksPort)),
+		Device:     "fd://" + strconv.Itoa(int(fd)),
+		LogLevel:   "debug",
 		UDPTimeout: time.Second * 10,
 	})
 	engine.Start()
@@ -74,9 +82,16 @@ func libxivpn_start(cConfig *C.char, socksPort C.int, fd C.int) *C.char {
 
 //export libxivpn_stop
 func libxivpn_stop() {
+	defer func() {
+		r := recover()
+		if r != nil {
+			log(fmt.Sprintf("panic: %v", r))
+		}
+	}()
+
 	log("stop")
 	engine.Stop()
-	if (xrayServer != nil) {
+	if xrayServer != nil {
 		xrayServer.Close()
 		xrayServer = nil
 	}
