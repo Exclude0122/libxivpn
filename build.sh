cfe9770c4f49128fba319fa12daf02dfe85b5deb
Exclude0122 <173652916+Exclude0122@users.noreply.github.com>
2025-04-14 01:30:17 -0400

    rename go to .go to fix interference from go mod tidy

--- build.sh
+++ build.sh
@@ -5,17 +5,18 @@
 # Download go and apply patch
 if [ "$2" = "patch" ]; then
 
-  if [ ! -d "go" ]; then
+  if [ ! -d ".go" ]; then
     echo "Downloading go..."
 
     rm go1.24.2.linux-amd64.tar.gz
-    wget https://go.dev/dl/go1.24.2.linux-amd64.tar.gz
+    curl -L https://go.dev/dl/go1.24.2.linux-amd64.tar.gz > go1.24.2.linux-amd64.tar.gz
     tar -C ./ -xzf go1.24.2.linux-amd64.tar.gz
+    mv go .go
 
     patch -d go -p1 -b < goruntime-boottime-over-monotonic.diff
   fi
 
-  export PATH="$(pwd)/go/bin":$PATH
+  export PATH="$(pwd)/.go/bin":$PATH
   echo "Go patched"
 fi
 
